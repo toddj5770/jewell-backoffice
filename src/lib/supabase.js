c449ea49749155b29c06eeeb233ce8ee1a2f9790
toddj5770 <todd@jewellre.com>
2026-04-11 23:07:19 -0400

    Add files via upload

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://tqpysubmkurdzrqlebwo.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxcHlzdWJta3VyZHpycWxlYndvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NTQ1NTIsImV4cCI6MjA5MTUzMDU1Mn0.I3gPaZo4gP8_h5JNTPkpcI5zFhjpUtRWzif2WcYZUxY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
